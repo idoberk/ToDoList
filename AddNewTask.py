e07afe1b548257302068bdc83e87a6821a18c62c
Ido Berkovits <berkovitsido@gmail.com>
2024-09-22 10:53:29 +0300

    Added TreeView

--- AddNewTask.py
+++ AddNewTask.py
@@ -1,11 +1,13 @@
+from datetime import *
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- TODO ------------------------------- #
-# todo 1: Refactor the open_new_window function so that the window will be configured based on the button pressed in the root window
-# todo 2: Refactor the submit_task function
-# todo 3: Refactor the class so that when creating a task, it will be an entire object (like in the ListRecord), with an 'empty' checkButton associated to each newly created task
+# strike todo 1: Refactor the open_new_window function so that the window will be configured based on the button pressed in the root window
+# strike todo 2: Refactor the submit_task function
+# strike todo 3: Refactor the class so that when creating a task, it will be an entire object (like in the ListRecord)
 # todo 4: Add descriptions to functions
-
+# todo 5: Close the add task window after submitting a new task (optional)
 # ---------------------------- CONSTANTS ------------------------------- #
 WIDGET_STICKY = "ew"
 X_PADDING = 5
@@ -17,47 +19,31 @@ class AddNewTask:
 		self.user_tasks_list = []
 		self.tasks_ID = 1
 
-	def submit_task(self, input_entry):
+	def update_task_id(self):
+		"""Increment taskID"""
+		self.tasks_ID += 1
+
+	def submit_task(self, input_entry, tree_view):
 		task_text = input_entry.get()
-		if task_text:
-			check_button = Checkbutton()
-			check_button.grid(column = 0, row = self.tasks_ID)
-			check_button.config(text = f'{task_text}')
-			self.tasks_ID += 1
-			self.user_tasks_list.append(check_button)
-		input_entry.delete(0, END)
-
-	def open_new_window(self, root):
-		add_task_window = Toplevel(root)
-		add_task_window.title("Add new task")
-		add_task_window.geometry("325x200")
-		add_task_window.config(padx = X_PADDING, pady = Y_PADDING)
-		add_task_window.resizable(False, False) # Disables window being able to be resized
-		add_task_window.grid_columnconfigure(0, weight = 1) # Makes column 0 expand
-		add_task_window.grid_columnconfigure(1, weight = 1) # Makes column 1 expand
-		add_task_window.grid_rowconfigure(2, weight = 1) # Makes row 2 expand (take extra space between the task_description_entry and the buttons)
-		root.attributes('-disabled', True) # Disabled root window
-
-		def on_close():
-			"""Re-enables the root window and closes the current window."""
-			root.attributes('-disabled', False)
-			add_task_window.destroy()
-
-		# ------ LABEL SETUP --------- #
-		description_label = Label(add_task_window, text = "Description:")
-		description_label.grid(column = 0, row = 0, padx = 1, sticky = "w")
-
-		# ------ ENTRY SETUP --------- #
-		task_description_entry = Entry(add_task_window)
-		task_description_entry.focus()
-		task_description_entry.grid(column = 0, row = 1, columnspan = 3, padx = X_PADDING, pady = Y_PADDING, sticky = WIDGET_STICKY)
-
-		# ------ BUTTON SETUP --------- #
-		submit_button = Button(add_task_window, width = BUTTONS_WIDTH, text = "Submit", command = lambda: self.submit_task(task_description_entry))
-		submit_button.grid(column = 0, row = 3, padx = X_PADDING, pady = Y_PADDING, sticky = WIDGET_STICKY)
-
-		cancel_button = Button(add_task_window, width = BUTTONS_WIDTH, text = "Cancel", command = on_close)
-		cancel_button.grid(column = 1, row = 3, padx = X_PADDING, pady = Y_PADDING, sticky = WIDGET_STICKY)
-
-		add_task_window.grab_set() # Disabled interaction with other windows
-		add_task_window.protocol("WM_DELETE_WINDOW", on_close) # Handles the window close event
+		task_created_time = datetime.now()
+		if task_text != "":
+			new_task = {
+				"TaskID": self.tasks_ID,
+				"Description": task_text,
+				"Created": task_created_time.strftime("%d-%m-%Y %H:%M:%S"),
+				# "Completed Time": "",
+				# "Completed": task_completed,
+				# "Task Completion Time": ""
+			}
+			input_entry.delete(0, END)
+			self.user_tasks_list.append(new_task)
+			tree_view.insert(
+				"",
+				END,
+				values = (f"{self.tasks_ID}", self.user_tasks_list[self.tasks_ID - 1]["TaskID"], self.user_tasks_list[self.tasks_ID - 1]['Description'], self.user_tasks_list[
+					self.tasks_ID - 1][
+					"Created"])
+			)
+			self.update_task_id()
+		else:
+			messagebox.showwarning(title = "", message = "Description can't be empty!")
--- main.py
+++ main.py
@@ -3,43 +3,73 @@
 # strike todo 2: Add task completed time and calculate the time it took to finish the task
 # strike todo 3: If user edits the task, it won't change the task created time
 # strike todo 4: Consider refactoring the code and add a new class per action (AddTask, DeleteTask, EditTask, MarkTaskAsCompleted)
-# todo 5: Create a new "WindowHandler" module that creates a new window when clicking on certain buttons
+# strike todo 5: Create a new "WindowHandler" module that creates a new window when clicking on certain buttons
 # todo 6: Add EditTask module to edit existing tasks (description, etc...)
 # todo 7: Add DeleteTask module to delete existing tasks
 # todo 8: Each action (Add / Delete / Edit...) will have a "unique" window configuration
 # todo 9: Add Exit button to the main window
 
 from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import Treeview
+
 from ListHandler import ListHandler
 from Menu import Menu, print_menu
 from AddNewTask import AddNewTask
+from WindowHandler import open_new_window
 
 # ---------------------------- CONSTANTS ------------------------------- #
-FONT = ("Ariel", 40, "italic")
+FONT = ("Ariel", 8, "bold")
 
 # ---------------------------- UI SETUP ------------------------------- #
 root = Tk()
 root.title("To Do List")
-root.geometry("600x400")
-root.resizable(False, False)
+root.geometry("920x400")
+root.resizable(True, False)
+tree_view = ttk.Treeview(columns = ("", "taskID", "description", "created"))
+vertical_scroll_bar = ttk.Scrollbar(root, orient = VERTICAL, command = tree_view.yview)
+horizontal_scroll_bar = ttk.Scrollbar(root, orient = HORIZONTAL, command = tree_view.xview)
+vertical_scroll_bar.grid(column = 1, row = 0, sticky = (N, S))
+horizontal_scroll_bar.grid(column = 0, row = 1, sticky = (W, E))
+tree_view['yscrollcommand'] = vertical_scroll_bar.set
+tree_view['xscrollcommand'] = horizontal_scroll_bar.set
+
+style = ttk.Style()
+style.configure("Treeview.Heading", font = FONT)
+style.configure('Treeview', rowheight = 30)
+tree_view.heading("#1", text = "")
+tree_view.column("#1", minwidth = 30, width = 30, anchor = "center")
+tree_view.heading("#2", text = "TaskID")
+tree_view.column("#2", minwidth = 60, width = 60, anchor = "center")
+tree_view.heading("#3", text = "Description")
+tree_view.column("#3", minwidth = 200, width = 680)
+tree_view.heading("#4", text = "Created")
+tree_view.column("#4", minwidth = 80, width = 130, anchor = "center")
+
+new_task = AddNewTask()  # FIXME: Find new location.
+
+# tree_view.insert(
+# 	"",
+# 	END,
+# 	values = (new_task.user_tasks_list[0]["TaskID"], new_task.user_tasks_list[0]['Description'], new_task.user_tasks_list[0]["Created"])
+# )
+tree_view['show'] = 'headings'
+tree_view.grid(column = 0, row = 0)
 
 # ------ BACKGROUND SETUP --------- #
 
-new_task = AddNewTask() # FIXME: Find new location.
+# new_task = AddNewTask() # FIXME: Find new location.
 
 
 # ------ ENTRY SETUP --------- #
 
 
 # ------ LABEL SETUP --------- #
-title_label = Label(text = f"To Do List", font = FONT)
-title_label.grid(column = 1, row = 0)
 
 
 # ------ BUTTON SETUP --------- #
-add_button = Button(text = "Add", command = lambda: new_task.open_new_window(root))
-add_button.grid(column = 3, row = 1)
-
+add_button = Button(text = "Add", command = lambda: open_new_window(root, "Add", new_task, tree_view))
+add_button.grid(column = 0, row = 2)
 
 # main_menu = Menu()
 # user_list = ListHandler()
@@ -47,26 +77,7 @@ add_button.grid(column = 3, row = 1)
 # check_button = Checkbutton(user_list.add_new_task())
 
 
-
 root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # while is_program_running:
 # 	user_selection = int(input(f"What would you like to do? "))
